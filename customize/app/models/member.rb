6ed6f7476c5dd5f1a8e2df5a4a64846ca180fddc
excross <ne250110@senshu-u.jp>
2014-12-12 20:51:54 +0900

    change model/member

--- customize/app/models/member.rb
+++ customize/app/models/member.rb
@@ -1,3 +1,35 @@
 class Member < ActiveRecord::Base
-  attr_accessible :login_id, :password, :name, :tel, :email, :admin
+  attr_accessible :id, :login_id, :password, :name, :tel, :email, :admin
+  attr_accessor :password, :password_confirmation
+
+  validates :password, presence: { on: :create },
+    confirmation: { allow_blank: true}
+
+  def password=(val)
+    if val.present?
+      self.hashed_password = BCrypt::Password.create(val)
+    end
+    @password = val
+  end
+
+  class << self
+    def search(query)
+      rel = order("number")
+      if query.present?
+        rel = rel.where("name LIKE ? OR full_name LIKE ?",
+                "%#{query}%", "%#{query}%")
+      end
+      rel
+    end
+
+    def authenticate(name, password)
+      member = find_by_name(name)
+      if member && member.hashed_password.present? &&
+         BCrypt::Password.new(member.hashed_password) == password
+        member
+      else
+        nil
+      end
+    end
+  end
 end
