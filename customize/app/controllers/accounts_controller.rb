2e0cddcdc4f5eeed60566345b81195bb7afe513e
kazuki <ne250091@senshu-u.jp>
2014-12-18 18:31:26 +0900

    accounts_controller

--- customize/app/controllers/accounts_controller.rb
+++ customize/app/controllers/accounts_controller.rb
@@ -5,4 +5,14 @@ class AccountsController < ApplicationController
   def edit
     @member = @current_member
   end
+
+  def update
+    @member = @current_member
+    @member.assign_attributes(params[:member])
+    if @member.save
+      redirect_to :edit_account, notice: "アカウント情報を更新しました。"
+    else
+      render "edit"
+    end
+  end
 end
