adaef70f4be4dfa48f3941c19e5be8e7aa559ae4
excross <ne250110@senshu-u.jp>
2015-01-08 04:51:57 +0900

    change orders seed

--- customize/db/seeds/development/orders.rb
+++ customize/db/seeds/development/orders.rb
@@ -2,10 +2,10 @@
 
 status = ["仮予約", "本予約", "済"]
 
-0.upto(9) do |i|
+0.upto(30) do |i|
   @order = Order.create(order_date: "2014/12/07",
                member_id: i%3,
-               receive_date: Time.mktime(2014, 12, (24 + i / 2), (i + 10), 30),
+               receive_date: Time.mktime(2015, 1, (1 + i / 2) % 31, (i + 10) % 24, 30),
                lunchbox_id: i%3,
                staple_id: [0,3].sample,
                main_id: [1,4].sample,
