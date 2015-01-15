06f347710a580ef1f7173a9a8e25aedba3cf6cea
kazuki <ne250091@senshu-u.jp>
2015-01-15 09:00:53 +0900

    new,create追加

--- customize/app/controllers/admin/members_controller.rb
+++ customize/app/controllers/admin/members_controller.rb
@@ -3,13 +3,25 @@
 class Admin::MembersController < Admin::Base
   # 会員一覧
   def index
-#    @members = Member.order("number").
     @members = Member.order("id")
-#      paginate(page: params[:page], per_page: 15)
   end
 
   # 会員情報の詳細
   def show
     @member = Member.find(params[:id])
+  end
+
+  def new
+    @member = Member.new
+  end
+
+  # 会員の新規登録
+  def create
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to :root
+    else
+      render "new"
+    end
   end
 end
