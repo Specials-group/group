7cac45242973f6997e9a2b2a5ba395c0bd4ea765
kazuki <ne250091@senshu-u.jp>
2015-01-15 18:26:37 +0900

    members_controllers追加

--- customize/app/controllers/members_controller.rb
+++ customize/app/controllers/members_controller.rb
@@ -1,15 +1,17 @@
 # coding: utf-8
 
-class Admin::MembersController < Admin::Base
-  # 会員一覧
-  def index
-#    @members = Member.order("number").
-    @members = Member.order("id")
-#      paginate(page: params[:page], per_page: 15)
+class MembersController < ApplicationController
+  def new
+    @member = Member.new
   end
 
-  # 会員情報の詳細
-  def show
-    @member = Member.find(params[:id])
+  # 会員の新規登録
+  def create
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to :root
+    else
+      render "new"
+    end
   end
 end
